c9ac277dc03bbe05d73f311e89ac864d7eda6b15
zzorn <zzorn@iki.fi>
2013-11-14 18:25:57 +0200

    Handle calling script from other directory.

--- push_release.sh
+++ push_release.sh
@@ -1,5 +1,8 @@
 # Pushes the contents of this repository, called automatically from release scripts using this repository.
 
-git add repository && git commit -m"New Release" && git push
+# Go to script dir
+cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+# Add repository dir to git and push
+git add repository && git commit -m"New Release" && git push
 
